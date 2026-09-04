d8d6d10d0712a629f69eacd82a80285a33160a57
Leonce Ngendakuriyo <ngendakuriyoleonce75@gmail.com>
2026-09-04 08:42:24 +0400

    feat: update social links to Leonce accounts

--- database/seeders/SocialLinkSeeder.php
+++ database/seeders/SocialLinkSeeder.php
@@ -9,8 +9,7 @@ class SocialLinkSeeder extends Seeder
 {
     public function run(): void
     {
-        SocialLink::create(['platform' => 'github', 'url' => 'https://github.com/tahssin', 'icon' => 'github', 'is_active' => true, 'order_column' => 1]);
-        SocialLink::create(['platform' => 'linkedin', 'url' => 'https://linkedin.com/in/tahssin', 'icon' => 'linkedin', 'is_active' => true, 'order_column' => 2]);
-        SocialLink::create(['platform' => 'twitter', 'url' => 'https://twitter.com/tahssin', 'icon' => 'twitter', 'is_active' => true, 'order_column' => 3]);
+        SocialLink::create(['platform' => 'github', 'url' => 'https://github.com/ngendakuriyoleonce', 'icon' => 'github', 'is_active' => true, 'order_column' => 1]);
+        SocialLink::create(['platform' => 'linkedin', 'url' => 'https://linkedin.com/in/leonce-ngendakuriyo', 'icon' => 'linkedin', 'is_active' => true, 'order_column' => 2]);
     }
 }
