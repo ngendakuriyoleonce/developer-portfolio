2a0ee762aecf1038f014c7297274853aeabb72dc
Leonce Ngendakuriyo <ngendakuriyoleonce75@gmail.com>
2026-09-04 08:42:24 +0400

    feat: set education to University of the Great Lakes

--- database/seeders/EducationSeeder.php
+++ database/seeders/EducationSeeder.php
@@ -10,25 +10,14 @@ class EducationSeeder extends Seeder
     public function run(): void
     {
         Education::create([
-            'institution' => 'Carnegie Mellon University Africa',
-            'degree' => 'Bachelor of Science',
-            'field_of_study' => 'Computer Science',
-            'description' => 'Studied computer science fundamentals including algorithms, data structures, software engineering, and web development.',
-            'start_date' => '2021-09-01',
-            'end_date' => '2025-06-30',
+            'institution' => 'University of the Great Lakes (UGL)',
+            'degree' => 'Bachelor of Management Information Systems',
+            'field_of_study' => 'Management Information Systems',
+            'description' => 'Studied management information systems, gaining a strong foundation in information technology, business management, and software development.',
+            'start_date' => '2019-09-01',
+            'end_date' => '2023-06-30',
             'is_current' => false,
             'order_column' => 1,
-        ]);
-
-        Education::create([
-            'institution' => 'Online Learning Platforms',
-            'degree' => 'Professional Certificates',
-            'field_of_study' => 'Web Development',
-            'description' => 'Completed various courses on Laravel, PHP, and modern web development practices through Udemy, Laracasts, and other platforms.',
-            'start_date' => '2022-01-01',
-            'end_date' => null,
-            'is_current' => true,
-            'order_column' => 2,
         ]);
     }
 }
